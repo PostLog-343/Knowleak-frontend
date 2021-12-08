7a0d1cb98172979dfe4ba060c39e3e6986cf3c98
melihcancilek <melihcanclk@hotmail.com>
2021-12-08 12:03:04 +0300

    getting courses from database and show them
    -admin configs done
    -admin docs added to the system
    -Couse Model created
    -Courses at database shown at index.html
    -these courses paginated meaning only 8 post will be shown at the page

--- knowleak_cont/knowleak/settings.py
+++ knowleak_cont/knowleak/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'django.contrib.admindocs'
 ]
 
 MIDDLEWARE = [
--- knowleak_cont/pages/models.py
+++ knowleak_cont/pages/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import DateTimeField
 
+class Course(models.Model):
+    "img = models.ImageField(upload_to='static/assets/img/posts')"
+    title = models.CharField(max_length=50)
+    briefInstr = models.TextField()
+    courseOwner = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    creationDate = DateTimeField(auto_now_add=True)
+    classTime = DateTimeField()
+
+    def __str__(self):
+        return self.title
