3d8d31feac9feb7b3b2295f409b923426f390d50
Melihcan Çilek <melihcanclk@hotmail.com>
2022-01-20 18:53:34 +0300

    .

--- knowleak/knowleak/settings.py
+++ knowleak/knowleak/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA FILES
